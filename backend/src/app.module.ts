f6c80cc318601fce207f608def7e5b271a6d957d
codeli4m <applecdos504@gmail.com>
2023-07-25 18:38:06 -0500

    basic setup for google auth :)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 // typeormmodule for ddbb configuration
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Users } from './users/users.entity';
-import { Form } from './form/entities/form.entity';
 import { DataSource } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { FormModule } from './form/form.module';
 import { QuestionModule } from './question/question.module';
 import { ResponseModule } from './response/response.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+// googlestrategy for auth
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
   imports: [
+    // ConfigModule relies on dotenv
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'postgres',
-      password: 'TryHackM3',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       database: 'rform',
       entities: [__dirname + '/**/**/*.entity.{js,ts}'],
       // synchronize: true,
@@ -29,9 +31,10 @@ import { ResponseModule } from './response/response.module';
     FormModule,
     QuestionModule,
     ResponseModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy],
 })
 export class AppModule {
   // DataSource and EntityManager objects will be available to inject across the entire project (without needing to import any modules)
